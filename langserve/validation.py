6ec7ceadc2184691328737f12734d54b98e0cef9
Eugene Yurtsev <eyurtsev@gmail.com>
2023-10-11 14:55:33 -0400

    Add in code documentation (#25)
    Add some in code documentation

--- langserve/validation.py
+++ langserve/validation.py
@@ -1,3 +1,21 @@
+"""Code to dynamically create pydantic models for validating requests and responses.
+
+Requests share the same basic shape of input, config, and kwargs.
+
+Invoke and Batch responses use an `output` key for the output, other keys may
+be added to the response at a later date.
+
+Responses for stream and stream_log are specified as those endpoints use
+a streaming response.
+
+Type information for input, config and output can be specified by the user
+per runnable. This type information will be used for validation of the input and
+output and will appear in the OpenAPI spec for the corresponding endpoint.
+
+Models are created with a namespace to avoid name collisions when hosting
+multiple runnables. When present the name collisions prevent fastapi from
+generating OpenAPI specs.
+"""
 from typing import List, Optional, Sequence, Union
 
 try:
@@ -7,15 +25,16 @@ except ImportError:
 
 from typing_extensions import Type, TypedDict
 
-InputValidator = Union[Type[BaseModel], type]
-# The following langchain objects are considered to be safe to load.
+# Type that is either a python annotation or a pydantic model that can be
+# used to validate the input or output of a runnable.
+Validator = Union[Type[BaseModel], type]
 
 # PUBLIC API
 
 
 def create_invoke_request_model(
     namespace: str,
-    input_type: InputValidator,
+    input_type: Validator,
     config: TypedDict,
 ) -> Type[BaseModel]:
     """Create a pydantic model for the invoke request."""
@@ -31,10 +50,10 @@ def create_invoke_request_model(
 
 def create_stream_request_model(
     namespace: str,
-    input_type: InputValidator,
+    input_type: Validator,
     config: TypedDict,
 ) -> Type[BaseModel]:
-    """Create a pydantic model for the invoke request."""
+    """Create a pydantic model for the stream request."""
     stream_request_model = create_model(
         f"{namespace}StreamRequest",
         input=(input_type, ...),
@@ -47,7 +66,7 @@ def create_stream_request_model(
 
 def create_batch_request_model(
     namespace: str,
-    input_type: InputValidator,
+    input_type: Validator,
     config: TypedDict,
 ) -> Type[BaseModel]:
     """Create a pydantic model for the batch request."""
@@ -63,7 +82,7 @@ def create_batch_request_model(
 
 def create_stream_log_request_model(
     namespace: str,
-    input_type: InputValidator,
+    input_type: Validator,
     config: TypedDict,
 ) -> Type[BaseModel]:
     """Create a pydantic model for the invoke request."""
@@ -86,9 +105,11 @@ def create_stream_log_request_model(
 
 def create_invoke_response_model(
     namespace: str,
-    output_type: InputValidator,
+    output_type: Validator,
 ) -> Type[BaseModel]:
     """Create a pydantic model for the invoke response."""
+    # The invoke response uses a key called `output` for the output, so
+    # other information can be added to the response at a later date.
     invoke_response_type = create_model(
         f"{namespace}InvokeResponse",
         output=(output_type, ...),
@@ -99,9 +120,11 @@ def create_invoke_response_model(
 
 def create_batch_response_model(
     namespace: str,
-    output_type: InputValidator,
+    output_type: Validator,
 ) -> Type[BaseModel]:
     """Create a pydantic model for the batch response."""
+    # The response uses a key called `output` for the output, so
+    # other information can be added to the response at a later date.
     batch_response_type = create_model(
         f"{namespace}BatchResponse",
         output=(List[output_type], ...),
