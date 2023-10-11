f3cf7967f1a7e6b4877dd977b1f9268ff3536d91
Harrison Chase <hw.chase.17@gmail.com>
2023-10-11 13:37:49 -0700

    add conversational retrieval chain

--- examples/conversational_retrieval_chain/server.py
+++ examples/conversational_retrieval_chain/server.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
-"""Example LangChain server exposes a retriever."""
+"""Example LangChain server exposes a conversational retrieval chain."""
 from fastapi import FastAPI
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 from langserve import add_routes
 
@@ -11,16 +13,20 @@ vectorstore = FAISS.from_texts(
 )
 retriever = vectorstore.as_retriever()
 
+model = ChatOpenAI()
+
+chain = ConversationalRetrievalChain.from_llm(model, retriever)
+
 app = FastAPI(
     title="LangChain Server",
     version="1.0",
     description="Spin up a simple api server using Langchain's Runnable interfaces",
 )
-# Adds routes to the app for using the retriever under:
+# Adds routes to the app for using the chain under:
 # /invoke
 # /batch
 # /stream
-add_routes(app, retriever)
+add_routes(app, chain)
 
 if __name__ == "__main__":
     import uvicorn
