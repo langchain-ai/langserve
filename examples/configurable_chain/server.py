15716b402c9bf55cbcd24fb63399933e9b35bf56
Eugene Yurtsev <eyurtsev@gmail.com>
2023-10-31 15:55:14 -0400

    x

--- examples/configurable_chain/server.py
+++ examples/configurable_chain/server.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python
-"""Example LangChain server exposes a chain composed of a prompt and an LLM."""
+"""Example of configurable runnables.
+
+This example shows how to use two options for configuration of runnables:
+
+1) Configurable Fields: Use this to specify values for a given initialization parameter
+2) Configurable Alternatives: Use this to specify complete alternative runnables
+"""
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
-from langchain.pydantic_v1 import BaseModel
 from langchain.schema.output_parser import StrOutputParser
 from langchain.schema.runnable import ConfigurableField
 
 from langserve import add_routes
 
 model = ChatOpenAI(temperature=0.5).configurable_alternatives(
-    ConfigurableField(id="llm", name="LLM"),
+    ConfigurableField(
+        id="llm",
+        name="LLM",
+        description=(
+            "Decide whether to use a high or a low temperature parameter for the LLM."
+        ),
+    ),
     high_temp=ChatOpenAI(temperature=0.9),
-    low_temp=ChatOpenAI(temperature=0.1, max_tokens=1),
+    low_temp=ChatOpenAI(temperature=0.1),
     default_key="medium_temp",
 )
 prompt = PromptTemplate.from_template(
     "tell me a joke about {topic}."
-).configurable_fields(
+).configurable_fields(  # Example of a configurable field
     template=ConfigurableField(
         id="prompt",
         name="Prompt",
-        description="The prompt to use. Must contain {topic}",
+        description="The prompt to use. Must contain {topic}.",
     )
 )
 chain = prompt | model | StrOutputParser()
@@ -44,20 +55,9 @@ app.add_middleware(
 )
 
 
-# The input type is automatically inferred from the runnable
-# interface; however, if you want to override it, you can do so
-# by passing in the input_type argument to add_routes.
-class ChainInput(BaseModel):
-    """The input to the chain."""
-
-    topic: str
-
-
-add_routes(app, chain, input_type=ChainInput, config_keys=["configurable"])
-
-# Alternatively, you can rely on langchain's type inference
-# to infer the input type from the runnable interface.
-# add_routes(app, chain)
+# Add routes requires you to specify which config keys are accepted
+# specifically, you must accept `configurable` as a config key.
+add_routes(app, chain, config_keys=["configurable"])
 
 if __name__ == "__main__":
     import uvicorn
