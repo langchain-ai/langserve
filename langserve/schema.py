40ff32809e2bb2c01d08bc88b3f9d2871c86a148
Jake Rachleff <jake@langchain.dev>
2023-10-31 17:31:55 -0700

    comments

--- langserve/schema.py
+++ langserve/schema.py
@@ -24,15 +24,36 @@ class CustomUserType(BaseModel):
 
 
 class SharedResponseMetadata(BaseModel):
+    """
+    Any response metadata should inherit from this class. Response metadata
+    represents non-output data that may be useful to some clients, but
+    ignorable to most. For example, the run_ids associated with each run
+    kicked off by the associated request.
+
+    SharedResponseMetadata is an abstraction to represent any metadata
+    representing a LangServe response shared across all outputs in said
+    response.
+    """
+
     pass
 
 
 class SingletonResponseMetadata(SharedResponseMetadata):
+    """
+    Represents response metadata used for just single input/output LangServe
+    responses.
+    """
+
     # Represents the parent run id for a given request
     run_id: str
 
 
 class BatchResponseMetadata(SharedResponseMetadata):
+    """
+    Represents response metadata used for batches of input/output LangServe
+    responses.
+    """
+
     # Represents each parent run id for a given request, in
     # the same order in which they were received
     run_ids: list[str]
