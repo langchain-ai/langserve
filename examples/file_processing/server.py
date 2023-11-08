0dfc764fc7302397c5e470db830e42c423cbe756
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-07 22:03:20 -0500

    x

--- examples/file_processing/server.py
+++ examples/file_processing/server.py
@@ -17,8 +17,8 @@ import base64
 from fastapi import FastAPI
 from langchain.document_loaders.blob_loaders import Blob
 from langchain.document_loaders.parsers.pdf import PDFMinerParser
-from langchain.schema.runnable import RunnableLambda
 from langchain.pydantic_v1 import Field
+from langchain.schema.runnable import RunnableLambda
 
 from langserve import CustomUserType, add_routes
 
