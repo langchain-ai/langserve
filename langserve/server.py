abe9bb79eacd40662ae30a3ffeda03271fa97faf
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-13 14:34:15 -0500

    Minor: language update in splash screen (#224)
    Minor language update

--- langserve/server.py
+++ langserve/server.py
@@ -306,11 +306,11 @@ def _setup_global_app_handlers(app: Union[FastAPI, APIRouter]) -> None:
 
         if _PYDANTIC_MAJOR_VERSION == 2:
             print()
-            print(f'{orange("OpenAPI Docs:")}: ', end="")
+            print(f'{orange("OpenAPI Docs:")} ', end="")
             print(
                 "Running with pydantic >= 2: OpenAPI docs for "
                 "invoke/batch/stream/stream_log` endpoints will not be "
-                "generated; but, API endpoints and playground will not be affected."
+                "generated; but, API endpoints and playground will work as expected."
             )
         print()
 
