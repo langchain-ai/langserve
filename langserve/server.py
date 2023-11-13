74c0be09163724fba8fbba5541855a5455b4c255
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-13 14:31:39 -0500

    x

--- langserve/server.py
+++ langserve/server.py
@@ -306,7 +306,7 @@ def _setup_global_app_handlers(app: Union[FastAPI, APIRouter]) -> None:
 
         if _PYDANTIC_MAJOR_VERSION == 2:
             print()
-            print(f'{orange("OpenAPI Docs:")}: ', end="")
+            print(f'{orange("OpenAPI Docs:")} ', end="")
             print(
                 "Running with pydantic >= 2: OpenAPI docs for "
                 "invoke/batch/stream/stream_log` endpoints will not be "
