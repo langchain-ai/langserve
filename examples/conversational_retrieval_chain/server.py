b53fa466b086e34be53fbaba98613687c66a6395
Eugene Yurtsev <eyurtsev@gmail.com>
2023-11-16 11:01:40 -0500

    Update conversational retriever chain example (#238)
    Update conversational retriever example

--- examples/conversational_retrieval_chain/server.py
+++ examples/conversational_retrieval_chain/server.py
@@ -1,24 +1,84 @@
 #!/usr/bin/env python
-"""Example LangChain server exposes a conversational retrieval chain."""
+"""Example LangChain server exposes a conversational retrieval chain.
+
+Follow the reference here:
+
+https://python.langchain.com/docs/expression_language/cookbook/retrieval#conversational-retrieval-chain
+
+To run this example, you will need to install the following packages:
+pip install langchain openai faiss-cpu tiktoken
+"""  # noqa: F401
+
+from operator import itemgetter
 from typing import List, Tuple
 
 from fastapi import FastAPI
-from langchain.chains import ConversationalRetrievalChain
 from langchain.chat_models import ChatOpenAI
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.pydantic_v1 import BaseModel, Field
+from langchain.prompts import ChatPromptTemplate
+from langchain.prompts.prompt import PromptTemplate
+from langchain.schema import format_document
+from langchain.schema.output_parser import StrOutputParser
+from langchain.schema.runnable import RunnableMap, RunnablePassthrough
 from langchain.vectorstores import FAISS
+from pydantic.v1 import BaseModel, Field
 
 from langserve import add_routes
 
+_TEMPLATE = """Given the following conversation and a follow up question, rephrase the 
+follow up question to be a standalone question, in its original language.
+
+Chat History:
+{chat_history}
+Follow Up Input: {question}
+Standalone question:"""
+CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(_TEMPLATE)
+
+ANSWER_TEMPLATE = """Answer the question based only on the following context:
+{context}
+
+Question: {question}
+"""
+ANSWER_PROMPT = ChatPromptTemplate.from_template(ANSWER_TEMPLATE)
+
+DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template(template="{page_content}")
+
+
+def _combine_documents(
+    docs, document_prompt=DEFAULT_DOCUMENT_PROMPT, document_separator="\n\n"
+):
+    """Combine documents into a single string."""
+    doc_strings = [format_document(doc, document_prompt) for doc in docs]
+    return document_separator.join(doc_strings)
+
+
+def _format_chat_history(chat_history: List[Tuple]) -> str:
+    """Format chat history into a string."""
+    buffer = ""
+    for dialogue_turn in chat_history:
+        human = "Human: " + dialogue_turn[0]
+        ai = "Assistant: " + dialogue_turn[1]
+        buffer += "\n" + "\n".join([human, ai])
+    return buffer
+
+
 vectorstore = FAISS.from_texts(
-    ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+    ["harrison worked at kensho"], embedding=OpenAIEmbeddings()
 )
 retriever = vectorstore.as_retriever()
 
-model = ChatOpenAI()
-
-chain = ConversationalRetrievalChain.from_llm(model, retriever)
+_inputs = RunnableMap(
+    standalone_question=RunnablePassthrough.assign(
+        chat_history=lambda x: _format_chat_history(x["chat_history"])
+    )
+    | CONDENSE_QUESTION_PROMPT
+    | ChatOpenAI(temperature=0)
+    | StrOutputParser(),
+)
+_context = {
+    "context": itemgetter("standalone_question") | retriever | _combine_documents,
+    "question": lambda x: x["standalone_question"],
+}
 
 
 # User input
@@ -32,9 +92,8 @@ class ChatHistory(BaseModel):
     question: str
 
 
-chain = ConversationalRetrievalChain.from_llm(model, retriever).with_types(
-    input_type=ChatHistory
-)
+conversational_qa_chain = _inputs | _context | ANSWER_PROMPT | ChatOpenAI()
+chain = conversational_qa_chain.with_types(input_type=ChatHistory)
 
 app = FastAPI(
     title="LangChain Server",
