0a892dc1bb7f8ade655027f2559ec3e84bcfa148
Tat Dat Duong <david@duong.cz>
2023-11-27 23:28:49 +0100

    Handle AIMessageChunk, attempt to gather non-dict values

--- langserve/playground/src/utils/path.ts
+++ langserve/playground/src/utils/path.ts
@@ -2,11 +2,18 @@ function isAccessibleObject(x: unknown): x is Record<string | number, unknown> {
   return typeof x === "object" && x != null;
 }
 
+export function getNormalizedJsonPath(
+  path: string | number | Array<string | number>
+) {
+  return Array.isArray(path) ? path : [path];
+}
+
 export function traverseNaiveJsonPath(
   x: unknown,
   path: string | number | Array<string | number>
 ) {
-  const queue = Array.isArray(path) ? path : [path];
+  const queue = getNormalizedJsonPath(path);
+
   let tmp: unknown = x;
   while (queue.length > 0) {
     const first = queue.shift()!;
