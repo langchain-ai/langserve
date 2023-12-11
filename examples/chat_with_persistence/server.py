fb49b833848c261c85b3ce05c2070aa91243ab40
Eugene Yurtsev <eyurtsev@gmail.com>
2023-12-10 22:03:38 -0500

    x

--- examples/chat_with_persistence/server.py
+++ examples/chat_with_persistence/server.py
@@ -5,7 +5,7 @@ For simplicity, we're using file storage here -- to avoid the need to set up
 a database. This is obviously not a good idea for a production environment,
 but will help us to demonstrate the RunnableWithMessageHistory interface.
 
-We'll use cookies to identify the user. This will help illustrate how to
+We'll use cookies to identify the user and/or session. This will help illustrate how to
 fetch configuration from the request.
 """
 import re
@@ -46,26 +46,16 @@ def create_session_factory(
     if not base_dir_.exists():
         base_dir_.mkdir(parents=True)
 
-    def get_chat_history(user_id: str, conversation_id: str) -> FileChatMessageHistory:
+    def get_chat_history(conversation_id: str) -> FileChatMessageHistory:
         """Get a chat history from a session ID."""
-        if not _is_valid_identifier(user_id):
-            raise ValueError(
-                f"User ID {user_id} is not in a valid format. "
-                "User ID must only contain alphanumeric characters, "
-                "hyphens, and underscores."
-            )
         if not _is_valid_identifier(conversation_id):
             raise ValueError(
-                f"Session ID {conversation_id} is not in a valid format. "
-                "Session ID must only contain alphanumeric characters, "
+                f"Conversation id {conversation_id} is not in a valid format. "
+                "Conversatino ID must only contain alphanumeric characters, "
                 "hyphens, and underscores."
             )
-
-        user_dir = base_dir_ / user_id
-        if not user_dir.exists():
-            user_dir.mkdir(parents=True)
-        file_path = user_dir / f"{conversation_id}.json"
-        return FileChatMessageHistory(file_path)
+        file_path = base_dir_ / f"{conversation_id}.json"
+        return FileChatMessageHistory(str(file_path))
 
     return get_chat_history
 
@@ -75,26 +65,6 @@ app = FastAPI(
     version="1.0",
     description="Spin up a simple api server using Langchain's Runnable interfaces",
 )
-
-
-def _per_request_config_modifier(
-    config: Dict[str, Any], request: Request
-) -> Dict[str, Any]:
-    """Update the config"""
-    config = config.copy()
-    configurable = config.get("configurable", {})
-    # Look for a cookie named "user_id"
-    user_id = request.cookies.get("user_id", None)
-
-    if user_id is None:
-        raise HTTPException(
-            status_code=400,
-            detail="No session ID found. Please set a cookie named 'session_id'.",
-        )
-
-    configurable["user_id"] = user_id
-    config["configurable"] = configurable
-    return config
 
 
 # Declare a chain
@@ -146,15 +116,6 @@ chain_with_history = RunnableWithMessageHistory(
 add_routes(
     app,
     chain_with_history,
-    per_req_config_modifier=_per_request_config_modifier,
-    # Disable playground and batch
-    # 1) Playground we're passing information via headers, which is not supported via
-    #    the playground right now.
-    # 2) Disable batch to avoid users being confused. Batch will work fine
-    #    as long as users invoke it with multiple configs appropriately, but
-    #    without validation users are likely going to forget to do that.
-    #    In addition, there's likely little sense in support batch for a chatbot.
-    disabled_endpoints=["playground", "batch"],
 )
 
 if __name__ == "__main__":
