6e806924d6910442ef15bf7d87c8fc49fe62b285
Eugene Yurtsev <eyurtsev@gmail.com>
2024-01-26 14:19:19 -0500

    x

--- tests/unit_tests/utils/llms.py
+++ tests/unit_tests/utils/llms.py
@@ -5,25 +5,19 @@ import time
 from typing import (
     Any,
     AsyncIterator,
-    Dict,
     Iterator,
     List,
     Mapping,
     Optional,
-    Union,
     cast,
 )
 
-from langchain_core.callbacks import (
-    AsyncCallbackManagerForLLMRun,
-    CallbackManagerForLLMRun,
-)
 from langchain_core.callbacks.manager import (
     AsyncCallbackManagerForLLMRun,
     CallbackManagerForLLMRun,
 )
 from langchain_core.language_models import LLM
-from langchain_core.language_models.chat_models import BaseChatModel, SimpleChatModel
+from langchain_core.language_models.chat_models import BaseChatModel
 from langchain_core.messages import (
     AIMessage,
     AIMessageChunk,
@@ -31,143 +25,6 @@ from langchain_core.messages import (
 )
 from langchain_core.outputs import ChatGeneration, ChatGenerationChunk, ChatResult
 from langchain_core.runnables import run_in_executor
-
-
-class FakeMessagesListChatModel(BaseChatModel):
-    """Fake ChatModel for testing purposes."""
-
-    responses: List[BaseMessage]
-    sleep: Optional[float] = None
-    i: int = 0
-
-    def _generate(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> ChatResult:
-        response = self.responses[self.i]
-        if self.i < len(self.responses) - 1:
-            self.i += 1
-        else:
-            self.i = 0
-        generation = ChatGeneration(message=response)
-        return ChatResult(generations=[generation])
-
-    @property
-    def _llm_type(self) -> str:
-        return "fake-messages-list-chat-model"
-
-
-class FakeListChatModel(SimpleChatModel):
-    """Fake ChatModel for testing purposes."""
-
-    responses: List
-    sleep: Optional[float] = None
-    i: int = 0
-    error_on_chunk_number: Optional[int] = None
-
-    @property
-    def _llm_type(self) -> str:
-        return "fake-list-chat-model"
-
-    def _call(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> str:
-        """First try to lookup in queries, else return 'foo' or 'bar'."""
-        response = self.responses[self.i]
-        if self.i < len(self.responses) - 1:
-            self.i += 1
-        else:
-            self.i = 0
-        return response
-
-    def _stream(
-        self,
-        messages: List[BaseMessage],
-        stop: Union[List[str], None] = None,
-        run_manager: Union[CallbackManagerForLLMRun, None] = None,
-        **kwargs: Any,
-    ) -> Iterator[ChatGenerationChunk]:
-        response = self.responses[self.i]
-        if self.i < len(self.responses) - 1:
-            self.i += 1
-        else:
-            self.i = 0
-        for i_c, c in enumerate(response):
-            if self.sleep is not None:
-                time.sleep(self.sleep)
-            if (
-                self.error_on_chunk_number is not None
-                and i_c == self.error_on_chunk_number
-            ):
-                raise Exception("Fake error")
-
-            yield ChatGenerationChunk(message=AIMessageChunk(content=c))
-
-    async def _astream(
-        self,
-        messages: List[BaseMessage],
-        stop: Union[List[str], None] = None,
-        run_manager: Union[AsyncCallbackManagerForLLMRun, None] = None,
-        **kwargs: Any,
-    ) -> AsyncIterator[ChatGenerationChunk]:
-        response = self.responses[self.i]
-        if self.i < len(self.responses) - 1:
-            self.i += 1
-        else:
-            self.i = 0
-        for i_c, c in enumerate(response):
-            if self.sleep is not None:
-                await asyncio.sleep(self.sleep)
-            if (
-                self.error_on_chunk_number is not None
-                and i_c == self.error_on_chunk_number
-            ):
-                raise Exception("Fake error")
-            yield ChatGenerationChunk(message=AIMessageChunk(content=c))
-
-    @property
-    def _identifying_params(self) -> Dict[str, Any]:
-        return {"responses": self.responses}
-
-
-class FakeChatModel(SimpleChatModel):
-    """Fake Chat Model wrapper for testing purposes."""
-
-    def _call(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[CallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> str:
-        return "fake response"
-
-    async def _agenerate(
-        self,
-        messages: List[BaseMessage],
-        stop: Optional[List[str]] = None,
-        run_manager: Optional[AsyncCallbackManagerForLLMRun] = None,
-        **kwargs: Any,
-    ) -> ChatResult:
-        output_str = "fake response"
-        message = AIMessage(content=output_str)
-        generation = ChatGeneration(message=message)
-        return ChatResult(generations=[generation])
-
-    @property
-    def _llm_type(self) -> str:
-        return "fake-chat-model"
-
-    @property
-    def _identifying_params(self) -> Dict[str, Any]:
-        return {"key": "fake"}
 
 
 class GenericFakeChatModel(BaseChatModel):
