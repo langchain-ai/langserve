d35da39f2e45d7d6fabd2ce9b2d26cf873a069a1
Eugene Yurtsev <eugene@langchain.dev>
2024-02-01 19:17:47 -0800

    x

--- examples/local_llm/server.py
+++ examples/local_llm/server.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python
 """Example LangChain Server that runs a local llm.
 
-**Attention** This is OK for prototyping / dev usage, but should not be used for production cases when there might be concurrent requests from different users. As of the time of writing, Ollama is designed for single user and cannot handle concurrent requests see this issue: https://github.com/ollama/ollama/issues/358
+**Attention** This is OK for prototyping / dev usage, but should not be used
+for production cases when there might be concurrent requests from different
+users. As of the time of writing, Ollama is designed for single user and cannot
+handle concurrent requests see this issue: 
+https://github.com/ollama/ollama/issues/358
+
+When deploying local models, make sure you understand whether the model is able
+to handle concurrent requests or not. If concurrent requests are not handled
+properly, the server will either crash or will just not be able to handle more
+than one user at a time.
 """
 from fastapi import FastAPI
 from langchain_community.chat_models import ChatOllama
