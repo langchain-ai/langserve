28df87b9aabdbf4ebcaa34ee2e65a72cdb891749
Jacob Lee <jacoblee93@gmail.com>
2024-03-08 11:22:14 -0800

    Update langserve/api_handler.py
    Co-authored-by: Eugene Yurtsev <eyurtsev@gmail.com>

--- langserve/api_handler.py
+++ langserve/api_handler.py
@@ -1405,7 +1405,7 @@ class APIHandler:
             raise HTTPException(
                 400,
                 "The feedback endpoint is only accessible when LangSmith is "
-                + "enabled on your LangServe server.",
+                + "enabled on your LangServe server."
                 + "Please set the proper environment variables",
             )
 
